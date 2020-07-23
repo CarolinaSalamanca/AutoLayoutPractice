e316ad2cf3fd225666467178f33dc4f37a6e9bfc
Carolina Salamanca <carolina@krasamo.com>
2020-07-23 10:22:38 -0500

    Initial Commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Carolina Salamanca on 7/23/20.
+//  Copyright © 2020 Carolina Salamanca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
